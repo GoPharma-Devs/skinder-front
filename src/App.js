27dd3c0fba954027727d07ebed2eaf82c711ee5d
Carlos Boyzo <carlosboyzo@MacBook-Pro-de-Carlos.local>
2022-04-24 13:15:37 -0500

    add rss buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './images/logo.png';
-import './App.css';
+import * as FaIcons from "react-icons/fa";
+
+
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div className="contenedor-botones">
+          <a href="https://www.facebook.com/skinder.mx"> <FaIcons.FaFacebook></FaIcons.FaFacebook></a>
 
+          <a href="https://www.instagram.com/skinder.mx/"><FaIcons.FaInstagram></FaIcons.FaInstagram></a>
+        </div>
       </header>
     </div>
   );
